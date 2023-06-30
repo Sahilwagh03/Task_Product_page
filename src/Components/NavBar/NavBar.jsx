1129c05c968444ab3ce4f364d532a7d4118a683e
sahilwagh03 <sahilwagh142@gmail.com>
2023-06-30 16:42:18 +0530

    fix

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,53 +1,65 @@
-import React, { useState } from 'react'
-import './NavBar.css'
-import menu from '../../assets/menu.svg'
-import close from '../../assets/close.svg'
-import Logo from '../../assets/nike_logo-removebg-preview.png'
-
+import React, { useState, useEffect } from 'react';
+import './NavBar.css';
+import menu from '../../assets/menu.svg';
+import close from '../../assets/close.svg';
+import Logo from '../../assets/nike_logo-removebg-preview.png';
 
 const NavBar = () => {
-    const [toggle, setToggle] = useState(false)
-    return (
-        <>
-            <div className='Nav_div'>
-                <nav className='navbar'>
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
-                    <img src={Logo} alt="" className='logo' />
-                    <div className='nav_menu'>
-                        <ul className='nav_lists'>
-                            <li className='nav_text'>MEN</li>
-                            <li className='nav_text'>WOMEN</li>
-                            <li className='nav_text'>KIDS</li>
-                            <li className='nav_text'>CUSTOMIZE</li>
-                            <li className='nav_text'>FAVOURIATE</li>
-                        </ul>
-                    </div>
-                    <div className='btn_div'>
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const isScrolled = scrollTop > 0;
+      setScrolled(isScrolled);
+    };
 
-                    </div>
+    window.addEventListener('scroll', handleScroll);
 
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-                    <div className='mobile_size'>
-                        <img onClick={() => setToggle(!toggle)} src={toggle ? close : menu} alt="" width={40} />
-
-                        <div className={` ${toggle ? 'flex' : 'hidden'} nav_mobile_menu`} >
-                            <ul className='nav_lists_mobile'>
-                                <li className='nav_text'>MEN</li>
-                                <li className='nav_text'>WOMEN</li>
-                                <li className='nav_text'>KIDS</li>
-                                <li className='nav_text'>CUSTOMIZE</li>
-                                <li className='nav_text'>FAVOURIATE</li>
-                            </ul>
-                            <div className='btn_div_mobile'>
-
-                            </div>
-                        </div>
-
-                    </div>
-                </nav>
+  return (
+    <>
+      <div className={`Nav_div ${scrolled ? 'scrolled' : ''}`}>
+        <nav className={`navbar ${scrolled ? 'bg-white' : ''}`}>
+          <img src={Logo} alt='' className='logo' />
+          <div className='nav_menu'>
+            <ul className='nav_lists'>
+              <li className={`nav_text ${scrolled ? 'text-black' : 'text-white'}`}>MEN</li>
+              <li className={`nav_text ${scrolled ? 'text-black' : 'text-white'}`}>WOMEN</li>
+              <li className={`nav_text ${scrolled ? 'text-black' : 'text-white'}`}>KIDS</li>
+              <li className={`nav_text ${scrolled ? 'text-black' : 'text-white'}`}>CUSTOMIZE</li>
+              <li className={`nav_text ${scrolled ? 'text-black' : 'text-white'}`}>FAVOURIATE</li>
+            </ul>
+          </div>
+          <div className='btn_div'></div>
+          <div className='mobile_size'>
+            <img
+              onClick={() => setToggle(!toggle)}
+              src={toggle ? close : menu}
+              alt=''
+              width={40}
+            />
+            <div className={` ${toggle ? 'flex' : 'hidden'} nav_mobile_menu`}>
+              <ul className='nav_lists_mobile'>
+                <li className={`nav_text ${scrolled ? 'text-black' : 'text-white'}`}>MEN</li>
+                <li className={`nav_text ${scrolled ? 'text-black' : 'text-white'}`}>WOMEN</li>
+                <li className={`nav_text ${scrolled ? 'text-black' : 'text-white'}`}>KIDS</li>
+                <li className={`nav_text ${scrolled ? 'text-black' : 'text-white'}`}>CUSTOMIZE</li>
+                <li className={`nav_text ${scrolled ? 'text-black' : 'text-white'}`}>FAVOURIATE</li>
+              </ul>
+              <div className='btn_div_mobile'></div>
             </div>
-        </>
-    )
-}
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
+
